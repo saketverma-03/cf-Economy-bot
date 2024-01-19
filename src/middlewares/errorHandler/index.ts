bb272c2436e62e0eb6bab0f1993efb45b0ec3153
saket verma <saketverma831@gmail.com>
2024-01-19 23:15:52 +0530

    improved error handling for oauth routes

--- src/middlewares/errorHandler/index.ts
+++ src/middlewares/errorHandler/index.ts
@@ -1,19 +1,28 @@
-
-import { Context } from "elysia"
+import { Context } from "elysia";
 
 type ErrorHandlerContext = Context & {
-  code: string,
-  error: Error
-}
+	code: string;
+	error: Error;
+};
 
-export const errorHandler = ({ code, error, set }: ErrorHandlerContext) => {
-  switch (code) {
-    case "AuthenticationError":
-      Response.json({ message: error.message })
-      break;
+export const errorHandler = ({
+	code,
+	error,
+	set,
+	path,
+}: ErrorHandlerContext) => {
+	switch (code) {
+		case "AuthenticationError":
+			return Response.json({ message: error.message });
 
-    default:
-      Response.json({ message: error.message, error: "uknown error" })
-      break;
-  }
-}
+		case "VALIDATION":
+			return Response.json({
+				type: "validation error",
+				message: `error in ${error.type} validation`,
+				values: error.value,
+			});
+
+		default:
+			return Response.json({ message: error.message, type: "Uknown Error" });
+	}
+};
--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -1,54 +1,48 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { config } from "@config/index";
 
+export const OauthRoute = new Elysia().get(
+	"/redirect",
+	async ({ set, query, cookie: { access_token, refresh_token } }) => {
+		const { code } = query;
 
-export const OauthRoute = new Elysia().get("/redirect", async ( { set, query,cookie:{ access_token, refresh_token} } ) => {
-
-	const { code } = query;
-
-	
-	try {
-		if (!code) {
-			throw new Error('Code Not Found');
-		}
-		const tokenResponseData = await fetch('https://discord.com/api/oauth2/token', {
-			method: 'POST',
-			body: new URLSearchParams({
-				client_id: config.env.DISCORD_CLIENT_ID,
-				client_secret: config.env.DISCORD_CLIENT_SECRET,
-				code,
-				grant_type: 'authorization_code',
-				redirect_uri: 'http://localhost:3000/redirect', // redirect URI is to be mentioned dynamically
-				scope: 'identify',
-			}),
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
+		const tokenResponseData = await fetch(
+			"https://discord.com/api/oauth2/token",
+			{
+				method: "POST",
+				body: new URLSearchParams({
+					client_id: config.env.DISCORD_CLIENT_ID,
+					client_secret: config.env.DISCORD_CLIENT_SECRET,
+					code,
+					grant_type: "authorization_code",
+					redirect_uri: "http://localhost:3000/redirect", // redirect URI is to be mentioned dynamically
+					scope: "identify",
+				}),
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
 			},
-		});
-		
+		);
+
 		const tokenResponse = await tokenResponseData.json();
+		if (tokenResponse.error) {
+			throw new Error("Error In Generating Auth token");
+		}
 
 		access_token.set({
 			value: tokenResponse.access_token,
-			expires: new Date(Date.now() + tokenResponse.expires_in*1000),
-		})
+			expires: new Date(Date.now() + tokenResponse.expires_in * 1000),
+		});
 
 		refresh_token.set({
 			value: tokenResponse.refresh_token,
-		})
-
+		});
 
 		set.redirect = "/dashboard";
-
-		return ;
-
-	} catch (error) {
-		// NOTE: An unauthorized token will not throw an error
-		// tokenResponseData.statusCode will be 401
-		console.error(error);
-		set.redirect = "/error";
-		return ;
-	}
-	
-}
+	},
+	{
+		query: t.Object({
+			code: t.String({ error: "FROM CODE DEF ERROR1" }),
+		}),
+	},
 );
