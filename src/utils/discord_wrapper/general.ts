ea3a7e1e8a843ea815b3df73cf2d903ab08025a1
saket verma <saketverma831@gmail.com>
2024-01-26 00:51:59 +0530

    update: fix image extension

--- src/utils/discord_wrapper/general.ts
+++ src/utils/discord_wrapper/general.ts
@@ -1,7 +1,7 @@
 // https://discord.com/developers/docs/reference#image-data
 export const discordImage = {
     getGuildIcon: (guildId: string | number, guildIcon: string | number) =>
-        `https://cdn.discordapp.com/icons/${guildId}/${guildIcon}.WebP `,
+        `https://cdn.discordapp.com/icons/${guildId}/${guildIcon}.webp `,
 
     getGuildBannerImage: (
         guildId: string | number,
@@ -14,4 +14,8 @@ export const discordImage = {
 
     getRoleIcon: (roleId: string | number, roleIcon: string | number) =>
         `https://cdn.discordapp.com/role-icons/${roleId}/${roleIcon}.WebP `,
+};
+
+export const generateBotInviteLink = (clinetId: string, guildId: string) => {
+    return `https://discord.com/oauth2/authorize?client_id=${clinetId}&permissions=8&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fonboard&scope=bot&guild_id=${guildId}&disable_guild_select=true`;
 };
