3fde454af817c319b4b01b14faf90491a8bd7ece
saket verma <saketverma831@gmail.com>
2024-01-26 00:48:27 +0530

    update model to allow the use of guild id as _id

--- src/db/models/community.ts
+++ src/db/models/community.ts
@@ -3,6 +3,7 @@ import mongoose, { InferSchemaType } from 'mongoose';
 const CommunitySchema = new mongoose.Schema(
     {
         // use guildId as _id
+        _id: { type: String, require: true },
         currencyName: String,
         balance: { type: Number, default: 100000 },
         taxPerTransaction: { type: Number, default: false },
