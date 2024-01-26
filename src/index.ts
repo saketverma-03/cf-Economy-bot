390d7a56446caa4bc3c64a719dff62499def9b23
saket verma <saketverma831@gmail.com>
2024-01-27 01:53:21 +0530

    updated iniit funtion

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,28 @@ import app from '@/routes/index';
 import { config } from '@config/index';
 import dbConnect from '@/db/dbConnect';
 
-dbConnect().then(() => {
-    app.onStart(() => {
-        if (config.env.NODE_ENV === 'devlopment') {
-            fetch('http://localhost:3001/restart');
-            console.log('🦊 Triggering Live Reload');
-        }
+app.onStart(() => {
+    if (config.env.NODE_ENV === 'devlopment') {
+        fetch('http://localhost:3001/restart');
+        console.log('🦊 Triggering Live Reload');
+    }
+})
+    .onStart(() => {
+        config.healthCheck();
+        // dbConnect()
     })
-        .onStart(() => {
-            config.healthCheck();
-        })
-        .listen(3000, () => console.log('STARTED: 3000'));
-});
+    .listen(3000, () => console.log('STARTED: 3000'));
+/**
+ * 
+app.onStart(() => {
+    if (config.env.NODE_ENV === 'devlopment') {
+        fetch('http://localhost:3001/restart');
+        console.log('🦊 Triggering Live Reload');
+    }
+})
+    .onStart(() => {
+        config.healthCheck();
+    })
+    .listen(3000, () => console.log('STARTED: 3000'));
+ *
+ * */
