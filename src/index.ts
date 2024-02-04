46fcf95201ce50c48db1e2cc19acc4fed01e89db
saket verma <saketverma831@gmail.com>
2024-02-04 23:47:55 +0530

    chore removed loadcommand in onStart

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import app from '@/routes/index';
 import { config } from '@config/index';
 import dbConnect from '@/db/dbConnect';
 import { logintoBot } from './discord';
-import { LoadCommands } from './discord/loader';
 
 app.onStart(() => {
     if (config.env.NODE_ENV === 'devlopment') {
@@ -13,23 +12,8 @@ app.onStart(() => {
     .onStart(() => {
         config.healthCheck();
         dbConnect();
-        LoadCommands('1185950578745028629');
         logintoBot();
     })
     .listen(config.env.PORT, () =>
         console.log(`Server started at ${config.env.PORT}`),
     );
-/**
- * 
-app.onStart(() => {
-    if (config.env.NODE_ENV === 'devlopment') {
-        fetch('http://localhost:3001/restart');
-        console.log('🦊 Triggering Live Reload');
-    }
-})
-    .onStart(() => {
-        config.healthCheck();
-    })
-    .listen(3000, () => console.log('STARTED: 3000'));
- *
- * */
